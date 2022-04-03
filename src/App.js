abe39c0ad6a51f3ee9011be3c77922edc1dc211b
jankar-with-tarek <avirus170@gmail.com>
2022-04-04 00:06:16 +0600

    added router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
-
+import Home from './components/Home/Home';
+import Review from './components/Review/Review';
+import Dashboard from './components/Dashboard/Dashboard';
+import About from './components/About/About';
+import Blogs from './components/Blogs/Blogs'
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header></Header>
+      {/* <Home></Home> */}
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/reviews' element={<Review></Review>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route></Route>
+      </Routes>
+      {/* <Review></Review> */}
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import image from '../../images/milkman.jpg';
+import './Home.css'
 const Home = () => {
     return (
-        <div>
-            <h1>this is home</h1>
+        <div className='home-page'>
+            <div className='description'>
+                <div>
+                    <h1>A reader lives a thousand </h1>
+                    <h1>lives before he dies</h1>
+                    <p>Milkman is a historical psychological fiction novel written by the Irish author Anna Burns. Set during The Troubles in Northern Ireland, the story follows an 18-year-old girl who is harassed by an older married man known as the "milkman".</p>
+                    <button>Read More</button>
+                </div>
+            </div>
+            <div className='homepage-img'>
+                <img src={image} alt="" />
+            </div>
         </div>
     );
 };
