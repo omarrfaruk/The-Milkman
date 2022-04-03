578153f80a34ba338b2255dfa609c1cfe2d5fcd6
jankar-with-tarek <avirus170@gmail.com>
2022-04-03 21:13:22 +0600

    added header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>MILKMAN</h1>
+            <nav className='navbar'>
+                <Link to='/'>HOME</Link>
+                <Link to='/reviews'>REVIEWS</Link>
+                <Link to='/dashboard'>DASHBOARD</Link>
+                <Link to='/blogs'>BLOGS</Link>
+                <Link to='/about'>ABOUT</Link>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
