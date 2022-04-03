01205247ec099531d110f66041aaae7b5f02a9db
jankar-with-tarek <avirus170@gmail.com>
2022-04-04 04:17:54 +0600

    added pic

--- src/components/SingleReview/SingleReview.js
+++ src/components/SingleReview/SingleReview.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './SingleReview.css'
 
 const SingleReview = ({ singleReview }) => {
-    const { name, ratings, description, date } = singleReview;
+    const { name, ratings, description, date, picture } = singleReview;
     return (
         <div className='review-details'>
             <div className='ratings'>
-                <div>
-                    <h3 className='reviewer-name'>{name}</h3>
-                    <p className='date'><small>{date}</small></p>
+                <div className='img-name'>
+                    <div>
+                        <img className='img-size' src={picture} alt="" />
+                    </div>
+                    <div>
+                        <h3 className='reviewer-name'>{name}</h3>
+                        <p className='date'><small>{date}</small></p>
+                    </div>
                 </div>
                 <p>Ratings: {ratings}</p>
             </div>
